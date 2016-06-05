b81a533113ef8fc4df4473a341fe92bf010a6fe8
Emory Merryman <emory.merryman@gmail.com>
2016-06-05 23:37:20 +0000

    fix init

--- bin/init.sh
+++ bin/init.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-
-init-ssh.sh &&
-init-git.sh &&
-init-gpg.sh &&
-pass init ${PASS_GPG_KEY_ID} &&
-pass git init &&
-pass git remote add origin ${PASS_GIT_URL} &&
-pass git fetch origin master &&
-pass git rebase origin/master &&
-ln --symbolic --force /opt/greenlobster/bin/post-commit.sh ${HOME}/.password-store/.git/hooks/post-commit &&
-git -C ${HOME}/bin pull origin develop &&
-${HOME}/bin/setup.sh &&
-node /opt/greenlobster/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
-true
+export PATH=${PATH}:/opt/greenlobster/bin &&
+    init-ssh.sh &&
+    init-git.sh &&
+    init-gpg.sh &&
+    init-pass.sh &&
+    node /opt/greenlobster/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
+    true
