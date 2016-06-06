d9bce67bf8a418529e577dd4348822c1476e33ae
Emory Merryman <emory.merryman@gmail.com>
2016-06-06 14:37:36 +0000

    add init-workspace

--- bin/init.sh
+++ bin/init.sh
@@ -5,5 +5,6 @@ export PATH=${PATH}:/opt/greenlobster/bin &&
     init-git.sh &&
     init-gpg.sh &&
     init-pass.sh &&
+    init-workspace.sh &&
     node /opt/greenlobster/c9sdk/server.js --listen 0.0.0.0 -w ${HOME}/workspace -p 8080 --auth ${USER}:${USER} &&
     true
