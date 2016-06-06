9fe7a7ddc4be3066f2af07f560134f681222f58c
Emory Merryman <emory.merryman@gmail.com>
2016-06-06 14:22:12 +0000

    trying to fix init

--- bin/install-workspace.sh
+++ bin/install-workspace.sh
@@ -4,4 +4,5 @@ mkdir ${HOME}/workspace &&
     mkdir ${HOME}/workspace/greenlobster &&
     git -C ${HOME}/workspace/greenlobster init &&
     git -C ${HOME}/workspace/greenlobster remote add origin git@github.com:desertedscorpion/greenlobster.git &&
+    ln --symbolic --force /opt/greenlobster/bin/post-commit.sh ${HOME}/workspace/greenlobster/.git/hooks/post-commit &&
     true
