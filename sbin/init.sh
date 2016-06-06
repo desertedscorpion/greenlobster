34289a770ca850715773e2279ae148906f9e07d4
Emory Merryman <emory.merryman@gmail.com>
2016-06-06 01:18:58 +0000

    fixed typo

--- sbin/init.sh
+++ sbin/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /opt/greenlobster/private.user &&
-    cp /opt/greenlobster/private/id_rsa /opt/greenlobster/private/secret.key /opt/greenlobster/private/owner.trus /opt/greenlobster/private.user &&
+    cp /opt/greenlobster/private/id_rsa /opt/greenlobster/private/secret.key /opt/greenlobster/private/owner.trust /opt/greenlobster/private.user &&
     chmod 0555 /opt/greenlobster/private.user &&
     chmod 0444 /opt/greenlobster/private.user/* &&
     su --login emory /opt/greenlobster/bin/init.sh  &&
